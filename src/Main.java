c4bc5f22ac00b0fdd52a854e59403c190c343fc1
ferranpg <ferranpratgrau@gmail.com>
2022-06-04 16:05:57 +0200

    Code finished.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,21 @@
-public class Main {
 
+import java.util.Arrays;
+import static java.lang.System.*;
+
+class Main {
+    public static void main(String[] args) {
+        int[] intMax = {2,4,7,0,34};
+
+
+        if (intMax.length > 1){
+            Arrays.sort(intMax);
+            out.println("\r\nMaximum: " + intMax[intMax.length-1]);
+        } else {
+            out.println("\r\nMaximum: " + intMax[0]);
+        }
+
+
+
+    }
 }
+
